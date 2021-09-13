2c19edd6fc905d035dbb0db68f24ba95475dfabf
Dr-Dawg <78480399+Dr-Dawg@users.noreply.github.com>
2021-09-13 10:52:23 +0200

    Update VS1053.cpp

--- src/VS1053.cpp
+++ src/VS1053.cpp
@@ -427,5 +427,5 @@ void VS1053::loadUserCode(const unsigned short* plugin, unsigned short plugin_si
  * Load the latest generic firmware patch
  */
 void VS1053::loadDefaultVs1053Patches() {
-   loadUserCode(PATCHES,sizeof(PATCHES)/sizeof(PATCHES[0]));
+   loadUserCode(PATCHES,PATCHES_SIZE);
 };
