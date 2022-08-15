c53fbc8bbfa30c9eb649b245bc705b0936e7f8ee
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-08-15 20:05:32 +0200

    Compilation for AVR and RP2040

--- src/VS1053SPI.h
+++ src/VS1053SPI.h
@@ -9,7 +9,7 @@
 #endif
 /**
  * @brief SPI Dirver for VS1053. We support different alternative implementations.
- * Outside of Arduino you need to provide your own implementation.
+ * Outside of Arduino you need to provide your own
  * 
  * @tparam Driver 
  */
@@ -19,7 +19,7 @@ class VS1053SPI {
   public:
     void beginTransaction() const{ driver.beginTransaction();}
     void endTransaction() const {driver.endTransaction();}
-    void set_speed(int speed){ driver.set_speed(speed);}
+    void set_speed(uint32_t speed){ driver.set_speed(speed);}
 
     inline void write(uint8_t data) const {driver.write(data);}
     inline void write16(uint16_t data) const {driver.write16(data);}
@@ -30,45 +30,6 @@ class VS1053SPI {
 
   protected:
     Driver driver;
-};
-
-#if defined(ARDUINO)
-
-/**
- * @brief Generic SPI Driver for Arduino
- * 
- */
-class VS1053SPIArduino {
-  protected:
-    SPISettings settings = SPISettings(200000, MSBFIRST, SPI_MODE0);
-
-  public:
-    void beginTransaction() const{ SPI.beginTransaction(settings);}
-    void endTransaction() const{SPI.endTransaction();}
-    void set_speed(int speed){ settings._clock=speed;}
-
-    inline void write(uint8_t data) const {
-        (void)SPI.transfer(data);
-    }
-
-    inline uint8_t transfer(uint8_t data) const {
-        return SPI.transfer(data);
-    }
-
-    inline void write16(uint16_t data) const {
-        (void)SPI.transfer16(data);
-    }
-
-    uint16_t read16(uint16_t port) {
-        return SPI.transfer16(port);
-    }
-
-    inline void write_bytes(const uint8_t * data, uint32_t size) const {
-        for (int i = 0; i < size; ++i) {
-            SPI.transfer(data[i]);
-        }
-    }
-
 };
 
 
@@ -84,7 +45,7 @@ class VS1053SPIESP32 {
   public:
     void beginTransaction() const { SPI.beginTransaction(settings);}
     void endTransaction() const {SPI.endTransaction();}
-    void set_speed(int speed){ settings._clock=speed;}
+    void set_speed(uint32_t speed){ settings._clock=speed;}
 
     inline void write(uint8_t data) const {
         SPI.write(data);
@@ -108,6 +69,48 @@ class VS1053SPIESP32 {
 
 };
 
-#endif
+#elif defined(ARDUINO)
+
+/**
+ * @brief Generic SPI Driver for Arduino
+ * 
+ */
+class VS1053SPIArduino {
+  protected:
+    uint32_t speed;
+
+  public:
+    void beginTransaction() const{ 
+        SPISettings settings(speed, MSBFIRST, SPI_MODE0);       
+        SPI.beginTransaction(settings);
+    }
+    void endTransaction() const{SPI.endTransaction();}
+    void set_speed(uint32_t speed){ this->speed = speed;}
+
+    inline void write(uint8_t data) const {
+        (void)SPI.transfer(data);
+    }
+
+    inline uint8_t transfer(uint8_t data) const {
+        return SPI.transfer(data);
+    }
+
+    inline void write16(uint16_t data) const {
+        (void)SPI.transfer16(data);
+    }
+
+    uint16_t read16(uint16_t port) {
+        return SPI.transfer16(port);
+    }
+
+    inline void write_bytes(const uint8_t * data, uint32_t size) const {
+        SPI.transfer((void*)data, size);
+        // for (int i = 0; i < size; ++i) {
+        //     SPI.transfer(data[i]);
+        // }
+    }
+
+};
+
 #endif
 
